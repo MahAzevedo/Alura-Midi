5d095106c5f0df311caf8824e7a2d210a194d32f
Mah <madavetto@gmail.com>
2023-04-14 18:24:16 -0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
-
-function tocaSomClap() {
-    document.queryselector('#som_tecla_clap').play();
+function tocaSomPom() {
+    document.queryselector('#som_tecla_Pom').play();
 }
 
 document.querySelector('.tecla_pom');
