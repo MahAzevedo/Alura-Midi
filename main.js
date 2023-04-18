fc0006e9fdfcb884dea8dd291119555d16676f97
Mah <madavetto@gmail.com>
2023-04-18 19:10:44 -0300

    update

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+/*
+automatizar as nossa funcionalidades, e principalmente trabalhar com muitos
+elementos de uma vez só com LISTA DE ELEMENTOS de uma vez só
+*/
+
+document.querySelectorAll('.tecla');
 
