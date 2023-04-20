bc7b2840366d726e4ac7875649fc21a4bfad6e24
Mah <madavetto@gmail.com>
2023-04-19 21:04:59 -0300

    update

--- main.js
+++ main.js
@@ -268,13 +268,13 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onClick = function () {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
